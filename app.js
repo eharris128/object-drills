6496a9c28bed04a0b92636fac73c922b756578fe
lewi-g <dr.lewi@gmail.com>
2017-06-14 16:03:04 -0400

    apply method etc..

--- app.js
+++ app.js
@@ -1,42 +1,81 @@
-const objTwo = {
-	meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
-}
-
-for (var y in objTwo) {
-	console.log(objTwo[y][3]);
-}
-
-// console.log(Object.keys(objTwo[3]));
-
-// const obj = {
-// 	foo: "foo",
-// 	bar: 2,
-// 	fum: 3, 
-// 	quux: 4, 
-// 	spam: 5,
+// const p1 = {
+// 	name: "Loren",
+// 	job_title: "Free Lancer",
 // }
 
-// for (let x in obj) {
-// 	console.log(x + obj[x]);
+// const p2 = {
+// 	name: "Jenn",
+// 	job_title: "web developer",
+// }
+
+// const p3 = {
+// 	name: "Nicole",
+// 	job_title: "Arrangements Abroad",
+// }
+
+// function makeFriendsArr( x, y, z) {
+// 	let friends = [];
+// 	friends.push.apply(friends, [x, y, z]);
+// 	return friends;
+// }
+// //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+// //console.log(obj2array(p1, p2, p3));
+// let realFriends= makeFriendsArr(p1, p2, p3)
+// //console.log(realFriends);
+
+// function convertToProfiles(arr){
+// 	arr.forEach(
+// 		function(profile) {for(let prop in profile) {
+// 			console.log(profile[prop])
+// 			}
+// 		}
+// 	)
 // }
 
 
-// var obj = {a: 1, b: 2, c: 3};
+// convertToProfiles([p1, p2, p3]);
+
+
+
+// // const objTwo = {
+// // 	meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+// // }
+
+// // for (var y in objTwo) {
+// // 	console.log(objTwo[y][3]);
+// // }
+
+// // console.log(Object.keys(objTwo[3]));
+
+// // const obj = {
+// // 	foo: "foo",
+// // 	bar: 2,
+// // 	fum: 3, 
+// // 	quux: 4, 
+// // 	spam: 5,
+// // }
+
+// // for (let x in obj) {
+// // 	console.log(x + obj[x]);
+// // }
+
+
+// // var obj = {a: 1, b: 2, c: 3};
     
-// for (var prop in obj) {
-//   console.log(`obj.${prop} = ${obj[prop]}`);
-// }
+// // for (var prop in obj) {
+// //   console.log(`obj.${prop} = ${obj[prop]}`);
+// // }
 
-// const loaf = {
-// 	flour: 300,
-// 	water: 210,
-// 	hydration: function() {
-// 		return (this.water/this.flour) * 100;
-// 	}
-// }
+// // const loaf = {
+// // 	flour: 300,
+// // 	water: 210,
+// // 	hydration: function() {
+// // 		return (this.water/this.flour) * 100;
+// // 	}
+// // }
 
-// console.log(Object.keys(loaf));
-// console.log(loaf.flour + ' ' + loaf.water);
+// // console.log(Object.keys(loaf));
+// // console.log(loaf.flour + ' ' + loaf.water);
 
-//console.log(loaf.hydration());
+// //console.log(loaf.hydration());
 
