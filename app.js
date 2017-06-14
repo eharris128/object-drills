603b90e87d9840209692e6fe2fed302d996bfe04
lewi-g <dr.lewi@gmail.com>
2017-06-14 15:03:48 -0400

    2nd ex

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-const loaf = {
-	flour: 300,
-	water: 210,
-	hydration: function() {
-		return (this.water/this.flour) * 100;
-	}
+const obj = {
+	foo: "foo",
+	bar: 2,
+	fum: 3, 
+	quux: 4, 
+	spam: 5,
 }
 
-console.log(Object.keys(loaf));
-console.log(loaf.flour + ' ' + loaf.water);
+for (let x in obj) {
+	console.log(x + obj[x]);
+}
 
-console.log(loaf.hydration());
+
+// var obj = {a: 1, b: 2, c: 3};
+    
+// for (var prop in obj) {
+//   console.log(`obj.${prop} = ${obj[prop]}`);
+// }
+
+// const loaf = {
+// 	flour: 300,
+// 	water: 210,
+// 	hydration: function() {
+// 		return (this.water/this.flour) * 100;
+// 	}
+// }
+
+// console.log(Object.keys(loaf));
+// console.log(loaf.flour + ' ' + loaf.water);
+
+//console.log(loaf.hydration());
+
